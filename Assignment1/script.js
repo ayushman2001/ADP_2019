5a71d7725e1fe6f90efbac90a77c3766bc4b2c68
Bhumanyu Goyal <42897290+bhu800@users.noreply.github.com>
2019-08-24 23:09:59 +0530

    added javascript code for adding two numbers

--- Assignment1/script.js
+++ Assignment1/script.js
@@ -1,1 +1,11 @@
+var num1 = document.getElementById("num1");
+var num2 = document.getElementById("num2");
+var result = document.getElementById("result");
 
+num1.addEventListener("input", add);
+num2.addEventListener("input", add)
+
+function add() {
+    document.getElementById("result").innerHTML = Number(num1.value) + Number(num2.value);
+    //document.getElementById("result").innerHTML ='Number(num1.value) + Number(num2.value)';
+}
